681b8bfa92441ad0cb017b23a13fc26399b2eaec
nohriter <nohriter@gmail.com>
2021-12-01 01:58:09 +0900

    feat: 저장된 식당 목록  랜덤으로 가져오기 구현
    아직 식당 데이터가 샘플 데이터고, 값을 가져오는 로직을 수정해야할듯함

--- src/program/swing/whatTodayEat/View.java
+++ src/program/swing/whatTodayEat/View.java
@@ -4,8 +4,10 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.Enumeration;
 
 public class View extends JFrame {
+    private Controller controller = new Controller();
 
     public View() {
         Container contentPane = getContentPane();
@@ -16,7 +18,7 @@ public class View extends JFrame {
         Icon distanceAllBasic = new ImageIcon("image/basic_all_btn.jpg");
         Icon distanceAllHover = new ImageIcon("image/hover_all_btn.jpg");
         Icon distanceAllSelected = new ImageIcon("image/selected_all_btn.jpg");
-        distanceButtons[0] = new JRadioButton(distanceAllBasic);
+        distanceButtons[0] = new JRadioButton(Distance.All.value(), distanceAllBasic);
         distanceButtons[0].setRolloverIcon(distanceAllHover); // 마우스 올리기
         distanceButtons[0].setSelectedIcon(distanceAllSelected); // 마우스 올리기
         distanceButtons[0].setLocation(20, 31);
@@ -28,7 +30,7 @@ public class View extends JFrame {
         Icon threeHundBasic = new ImageIcon("image/basic_300_btn.jpg");
         Icon threeHundHover = new ImageIcon("image/hover_300_btn.jpg");
         Icon threeHundSelected = new ImageIcon("image/selected_300_btn.jpg");
-        distanceButtons[1] = new JRadioButton(threeHundBasic);
+        distanceButtons[1] = new JRadioButton(Distance.THREE_HUNDRED.value(), threeHundBasic);
         distanceButtons[1].setRolloverIcon(threeHundHover); // 마우스 올리기
         distanceButtons[1].setSelectedIcon(threeHundSelected); // 마우스 올리기
         distanceButtons[1].setLocation(146, 31);
@@ -40,7 +42,7 @@ public class View extends JFrame {
         Icon fiveHundBasic = new ImageIcon("image/basic_500_btn.jpg");
         Icon fiveHundHover = new ImageIcon("image/hover_500_btn.jpg");
         Icon fiveHundSelected = new ImageIcon("image/selected_500_btn.jpg");
-        distanceButtons[2] = new JRadioButton(fiveHundBasic);
+        distanceButtons[2] = new JRadioButton(Distance.FIVE_HUNDRED.value(), fiveHundBasic);
         distanceButtons[2].setRolloverIcon(fiveHundHover); // 마우스 올리기
         distanceButtons[2].setSelectedIcon(fiveHundSelected); // 마우스 올리기
         distanceButtons[2].setLocation(272, 31);
@@ -52,7 +54,7 @@ public class View extends JFrame {
         Icon oneKiloMBasic = new ImageIcon("image/basic_1km_btn.jpg");
         Icon oneKiloMHover = new ImageIcon("image/hover_1km_btn.jpg");
         Icon oneKiloMSelected = new ImageIcon("image/selected_1km_btn.jpg");
-        distanceButtons[3] = new JRadioButton(oneKiloMBasic);
+        distanceButtons[3] = new JRadioButton(Distance.ONE_KILOMETER.value(), oneKiloMBasic);
         distanceButtons[3].setRolloverIcon(oneKiloMHover); // 마우스 올리기
         distanceButtons[3].setSelectedIcon(oneKiloMSelected); // 마우스 올리기
         distanceButtons[3].setLocation(398, 31);
@@ -67,7 +69,7 @@ public class View extends JFrame {
         Icon foodAllBasic = new ImageIcon("image/food_basic_all_btn.jpg");
         Icon foodAllHover = new ImageIcon("image/food_hover_all_btn.jpg");
         Icon foodAllSelected = new ImageIcon("image/food_selected_all_btn.jpg");
-        foodButtons[0] = new JRadioButton(foodAllBasic);
+        foodButtons[0] = new JRadioButton(FoodCategory.ALL.value(), foodAllBasic);
         foodButtons[0].setRolloverIcon(foodAllHover); // 마우스 올리기
         foodButtons[0].setSelectedIcon(foodAllSelected); // 마우스 올리기
         foodButtons[0].setLocation(20, 100);
@@ -79,7 +81,7 @@ public class View extends JFrame {
         Icon foodKoreaBasic = new ImageIcon("image/food_basic_korea_btn.jpg");
         Icon foodKoreaHover = new ImageIcon("image/food_hover_korea_btn.jpg");
         Icon foodKoreaSelected = new ImageIcon("image/food_selected_korea_btn.jpg");
-        foodButtons[1] = new JRadioButton(foodKoreaBasic);
+        foodButtons[1] = new JRadioButton(FoodCategory.KOREA.value(), foodKoreaBasic);
         foodButtons[1].setRolloverIcon(foodKoreaHover); // 마우스 올리기
         foodButtons[1].setSelectedIcon(foodKoreaSelected); // 마우스 올리기
         foodButtons[1].setLocation(120, 100);
@@ -91,7 +93,7 @@ public class View extends JFrame {
         Icon foodChinaBasic = new ImageIcon("image/food_basic_china_btn.jpg");
         Icon foodChinaHover = new ImageIcon("image/food_hover_china_btn.jpg");
         Icon foodChinaSelected = new ImageIcon("image/food_selected_china_btn.jpg");
-        foodButtons[2] = new JRadioButton(foodChinaBasic);
+        foodButtons[2] = new JRadioButton(FoodCategory.CHINA.value(), foodChinaBasic);
         foodButtons[2].setRolloverIcon(foodChinaHover); // 마우스 올리기
         foodButtons[2].setSelectedIcon(foodChinaSelected); // 마우스 올리기
         foodButtons[2].setLocation(220, 100);
@@ -103,7 +105,7 @@ public class View extends JFrame {
         Icon foodJapanBasic = new ImageIcon("image/food_basic_japan_btn.jpg");
         Icon foodJapanHover = new ImageIcon("image/food_hover_japan_btn.jpg");
         Icon foodJapanSelected = new ImageIcon("image/food_selected_japan_btn.jpg");
-        foodButtons[3] = new JRadioButton(foodJapanBasic);
+        foodButtons[3] = new JRadioButton(FoodCategory.JAPAN.value(), foodJapanBasic);
         foodButtons[3].setRolloverIcon(foodJapanHover); // 마우스 올리기
         foodButtons[3].setSelectedIcon(foodJapanSelected); // 마우스 올리기
         foodButtons[3].setLocation(320, 100);
@@ -115,7 +117,7 @@ public class View extends JFrame {
         Icon foodForeignBasic = new ImageIcon("image/food_basic_foreign_btn.jpg");
         Icon foodForeignHover = new ImageIcon("image/food_hover_foreign_btn.jpg");
         Icon foodForeignSelected = new ImageIcon("image/food_selected_foreign_btn.jpg");
-        foodButtons[4] = new JRadioButton(foodForeignBasic);
+        foodButtons[4] = new JRadioButton(FoodCategory.FOREIGN.value(), foodForeignBasic);
         foodButtons[4].setRolloverIcon(foodForeignHover); // 마우스 올리기
         foodButtons[4].setSelectedIcon(foodForeignSelected); // 마우스 올리기
         foodButtons[4].setLocation(420, 100);
@@ -124,18 +126,20 @@ public class View extends JFrame {
         foodGroup.add(foodButtons[4]);
         contentPane.add(foodButtons[4]);
 
-        JTextField jTextField = new JTextField();
-        jTextField.setEnabled(false);
-        jTextField.setLocation(20, 166);
-        jTextField.setSize(484,252);
-        contentPane.add(jTextField);
+        JLabel jLabel = new JLabel("", SwingConstants.CENTER);
+        jLabel.setFont(new Font("맑은 고딕", Font.BOLD, 50));
+        jLabel.setLocation(20, 166);
+        jLabel.setSize(484, 252);
+        jLabel.setBorder(BorderFactory.createLineBorder(Color.black));
+        jLabel.setHorizontalAlignment(JLabel.CENTER);
+        contentPane.add(jLabel);
 
         Icon randomBasic = new ImageIcon("image/basic_random_btn.jpg");
         Icon randomHover = new ImageIcon("image/hover_random_btn.jpg");
         JButton randomBasicButton = new JButton(randomBasic);
         randomBasicButton.setRolloverIcon(randomHover);
         randomBasicButton.setLocation(20, 444);
-        randomBasicButton.setSize(486,90);
+        randomBasicButton.setSize(486, 90);
         randomBasicButton.setBorderPainted(false);
         randomBasicButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
         contentPane.add(randomBasicButton);
@@ -143,7 +147,14 @@ public class View extends JFrame {
         randomBasicButton.addMouseListener(new MouseAdapter() {
             @Override
             public void mousePressed(MouseEvent e) {
+                String selectedDistance = whatIsSelected(distanceGroup);
+                String selectedFood = whatIsSelected(foodGroup);
 
+                System.out.println(selectedDistance + " " + selectedFood);
+
+                Restaurant restaurant = controller.getRestaurant(selectedDistance, selectedFood);
+
+                jLabel.setText(restaurant.getName());
             }
         });
 
@@ -153,6 +164,18 @@ public class View extends JFrame {
         setSize(544, 600);
         setLocationRelativeTo(null);
         setVisible(true);
+    }
+
+    private String whatIsSelected(ButtonGroup buttonGroup) {
+        for (Enumeration<AbstractButton> buttons = buttonGroup.getElements(); buttons.hasMoreElements(); ) {
+            AbstractButton button = buttons.nextElement();
+
+            if (button.isSelected()) {
+                return button.getText();
+            }
+        }
+
+        return null;
     }
 
     public static void main(String[] args) {
